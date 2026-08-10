6a9cf6123910619862cd8aa5ec067da7e17797ee
nivedsanthosholiyil-cyber <nivedsanthosholiyil@gmail.com>
2026-08-11 00:10:20 +0530

    ]

---
+++ adiue.py
@@ -0,0 +1,10 @@
+import inflect
+p = inflect.engine()
+names =  []
+while True:
+    try:
+        name= input("items:")
+        names.append(name)
+    except EOFError:
+        break 
+print("Adieu, adieu, to " + p.join(names))       
