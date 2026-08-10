d1dc380b6d63a654f40ebfdccad69f94c70a85e3
nivedsanthosholiyil-cyber <nivedsanthosholiyil@gmail.com>
2026-08-10 21:27:34 +0530

    Add guessing game

--- s.py
+++ s.py
@@ -1,7 +1,18 @@
-
-
 import sys
-import cowsay
+import pyfiglet
+import random
 
-if len(sys.argv) == 2:
-    cowsay.cow("hello, " + sys.argv[1])
+fonts = pyfiglet.FigletFont.getFonts()
+
+if len(sys.argv) == 1:
+    font = random.choice(fonts)
+
+elif len(sys.argv) == 3 and sys.argv[1] in ["-f", "--font"] and sys.argv[2] in fonts:
+    font = sys.argv[2]
+
+else:
+    sys.exit("Invalid usage")
+
+text = input("Input: ")
+
+print(pyfiglet.figlet_format(text, font=font))
