26425283282947cebcb457759878b5bac9c0667c
U-DESKTOP-1RTSO6V\User <nivedsanthosholiyil@gmail.com>
2026-08-13 16:45:14 +0530

    67

---
+++ c/def.c
@@ -0,0 +1,15 @@
+#include <cs50.h>
+#include <stdio.h>
+void def(int t);
+int main(void)
+{
+    int n = get_int("how much 67 u want yo");
+    def(n);
+}
+void def(int t)
+{
+    for(int i=0 ;i < t; i++)
+    {
+        printf("67 cocomelon stairway to heaven \n");
+    }
+}
--- c/mew.c
+++ c/mew.c
@@ -1,10 +1,27 @@
 #include <stdio.h>
+#include <cs50.h>
+#include <stdbool.h>
 int main(void)
 
 {
-    for (int i = 0; i<3; i++)
+    int n;
+    while (true)
     {
-        printf("bitch ass nigger\n");
-        i--;
+        n = get_int("how much cuh u wnat ");
+        if (n<0)
+        {
+            continue;
+        
+        }
+        else
+        {
+            break;
+
+        }
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        printf("67\n");
     }
 }
