3464f59c6b55e3e4d06b854784ddf24a93248e6c
nivedsanthosholiyil-cyber <nivedsanthosholiyil@gmail.com>
2026-08-19 19:14:07 +0530

    yp

---
+++ python/calcu.py
@@ -0,0 +1,9 @@
+def main():
+    x = int(input("whast x"))
+    print("x squred is" , squre(x))
+
+
+def squre(n):
+    return n*n
+if __name__ == "__main__":
+    main()
---
+++ python/calcutes.py
@@ -0,0 +1,10 @@
+from calcu import squre
+
+def main():
+    tst()
+
+def tst():
+    assert squre(2) == 4
+    assert squre(3) == 9
+if __name__ == "__main__":
+    main()
