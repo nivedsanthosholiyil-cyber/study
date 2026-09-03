2969d1c81de53b01fd489d8638b1323d84d533ff
U-DESKTOP-1RTSO6V\User <nivedsanthosholiyil@gmail.com>
2026-09-03 22:43:49 +0530

    shw

---
+++ c/hole.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("    / l \n");
+    printf("    /   l \n");
+    printf("   /     l \n");
+    printf("  /       l \n");
+    return 0;
+}
